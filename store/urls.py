87ef3d8d5f21cee33ccb4717abac973b6fc35e25
Aleja <afajardo04@misena.edu.co>
2023-04-19 16:30:34 -0500

    funcion registrar,salir e ingresar usuario

--- store/urls.py
+++ store/urls.py
@@ -6,4 +6,6 @@ urlpatterns= [
     path("cart/",views.cart, name="cart"),
     path("checkout/",views.checkout, name="checkout"),
     path("registrar/",views.registrar, name="registrar"),
+    path("ingresar/",views.ingresar, name="ingresar"),
+    path("salir/",views.salir, name="salir"),
 ]
--- store/views.py
+++ store/views.py
@@ -1,12 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from store.models import *
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .form import UserRegisterForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+
+def salir(request):
+    logout(request)
+    messages.success(request, "Sesion Cerrada")
+    return redirect("ingresar")
+
+def ingresar(request):
+    if request.method=="POST":
+        form=AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            nombreUsuario=form.cleaned_data.get("username")
+            contraseña=form.cleaned_data.get("password")
+            usuario=authenticate(username=nombreUsuario, password=contraseña)
+            if usuario is not None:
+                login(request, usuario)
+                messages.success(request,"Bienvenido nuevamente %s" %nombreUsuario)
+                return redirect("store")
+            else:
+                messages.error(request, "El usuario o contraseña son incorrectos")
+        else:
+            messages.error(request, "El usuario o contraseña son incorrectos")
+    else:
+        form=AuthenticationForm()
+        context={"formulario":form}
+        return render(request, "store/login.html",context)
 
 def registrar(request):
     if request.method=="POST":
-        form=UserCreationForm(request.POST)
+        form=UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username=form.cleaned_data["username"]
